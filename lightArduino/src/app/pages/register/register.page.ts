9a52ca3ad1c315510192c902bf057505b09697ac
piercethevie <50924876+cb161769@users.noreply.github.com>
2020-12-22 12:12:52 -0400

    form de registrarse

--- lightArduino/src/app/pages/register/register.page.ts
+++ lightArduino/src/app/pages/register/register.page.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  userName:any;
+  userEmail:any;
+  userPassword:any;
+  userFirstName:any;
+  userLastName:any;
 
   constructor() { }
 
