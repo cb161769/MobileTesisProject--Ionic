72f667ab872006ad50b7cb2a3c0f576a42bdf2ad
piercethevie <50924876+cb161769@users.noreply.github.com>
2020-12-23 01:38:06 -0400

    home slides

--- lightArduino/src/app/pages/home/home.page.ts
+++ lightArduino/src/app/pages/home/home.page.ts
@@ -6,6 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  slides = [
+    {
+      img:'../../../assets/icon/save-energy.svg',
+      title:'Bienvenido a la aplicación Móvil <br> he aquí ciertos beneficios de la Aplicación:'
+    },
+    {
+      img: '../../../assets/icon/budget.svg',
+      title:'Seguimiento del consumo eléctrico <br> a través de Distintos Gráficos'
+    },
+    {
+      img: '../../../assets/icon/save-energy2.svg',
+      title:'Conocimiento del costo aproximado de su <br> consumo eléctrico'
+    },
+    {
+      img: '../../../assets/icon/iot.svg',
+      title:'Constantes actualizaciones sobre su comsumo a traves <br> del IOT'
+    }
+  ];
 
   constructor() { }
 
