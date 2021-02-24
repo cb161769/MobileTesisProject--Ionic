64bc6eeb19386c878ba8bd82d0858f3b29049114
piercethevie <50924876+cb161769@users.noreply.github.com>
2021-02-23 23:43:29 -0400

    fix bug - no data

--- lightArduino/src/app/pages/your-account/your-account.page.ts
+++ lightArduino/src/app/pages/your-account/your-account.page.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoadingController,ToastController,AlertController,NavController } from '@ionic/angular';
+import { Apollo } from 'apollo-angular';
+import { AwsAmplifyService } from 'src/app/data-services/aws-amplify.service';
+import { DynamoDBAPIService } from 'src/app/data-services/dynamo-db-api.service';
+import { EnergyService } from 'src/app/data-services/energyService/energy.service';
+import { MessageService } from 'src/app/data-services/messageService/message.service';
 
 @Component({
   selector: 'app-your-account',
   templateUrl: './your-account.page.html',
   styleUrls: ['./your-account.page.scss'],
 })
-export class YourAccountPage implements OnInit {
+export class YourAccountPage implements OnInit,OnDestroy {
 
-  constructor() { }
+  constructor(public awsAmplifyService: AwsAmplifyService,public loadingIndicator:LoadingController,public router:Router,
+    public DynamoDBService: DynamoDBAPIService, public ToastController : ToastController,public messageService:MessageService, public alertController: AlertController, public energyService:EnergyService,private apolloClient: Apollo, public navController:NavController) { }
 
   ngOnInit() {
+  }
+  ngOnDestroy(){
+
   }
   singOut():void{
 
