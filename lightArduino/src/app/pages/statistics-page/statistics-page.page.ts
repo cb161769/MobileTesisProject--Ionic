58aa4ef893590d357c42d6be5e4574b17223f449
piercethevie <50924876+cb161769@users.noreply.github.com>
2021-03-01 00:09:11 -0400

    changes to load relays and apply configuration

--- lightArduino/src/app/pages/statistics-page/statistics-page.page.ts
+++ lightArduino/src/app/pages/statistics-page/statistics-page.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoadingController, AlertController, NavController,ToastController } from '@ionic/angular';
+import { Apollo } from 'apollo-angular';
+import { AwsAmplifyService } from 'src/app/data-services/aws-amplify.service';
+import { DynamoDBAPIService } from 'src/app/data-services/dynamo-db-api.service';
+import { EnergyService } from 'src/app/data-services/energyService/energy.service';
+import { MessageService } from 'src/app/data-services/messageService/message.service';
 
 @Component({
   selector: 'app-statistics-page',
@@ -7,7 +14,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticsPagePage implements OnInit {
 
-  constructor() { }
+  constructor(public awsAmplifyService: AwsAmplifyService,public loadingIndicator:LoadingController,public router:Router,
+    public DynamoDBService: DynamoDBAPIService, public ToastController : ToastController,public messageService:MessageService, public alertController: AlertController, public energyService:EnergyService,private apolloClient: Apollo, public navController:NavController) { }
 
   ngOnInit() {
   }
@@ -21,6 +29,9 @@ export class StatisticsPagePage implements OnInit {
       console.log('Async operation has ended');
       event.target.complete();
     }, 2000);
+  }
+  async ionViewDidEnter(){
+   
   }
 
 }
