e571a733cc8bf91b70b49cf84b9cec69da6f7dbc
piercethevie <50924876+cb161769@users.noreply.github.com>
2021-03-06 01:07:14 -0400

    advance Connection One

--- lightArduino/src/app/pages/connection-one-tabs/connection-one-tabs-routing.module.ts
+++ lightArduino/src/app/pages/connection-one-tabs/connection-one-tabs-routing.module.ts
@@ -5,8 +5,19 @@ import { ConnectionOneTabsPage } from './connection-one-tabs.page';
 
 const routes: Routes = [
   {
-    path: '',
-    component: ConnectionOneTabsPage
+    path: 'connection-one-tab',
+    component: ConnectionOneTabsPage,
+    children: [
+      {
+        path:'tab1',
+        loadChildren:() => import('../connexion1/connexion1.module').then( m => m.Connexion1PageModule)
+      }
+    ]
+  },
+  {
+    path:'',
+    redirectTo:'connection-one-tab/tab1',
+    pathMatch:'full'
   }
 ];
 
