c88fe166ecd2ed47e7faf8170d9f84fcb0371159
piercethevie <50924876+cb161769@users.noreply.github.com>
2021-03-17 00:29:48 -0400

    some validations ad UPDATE / Create Device Configs

--- lightArduino/src/app/pages/edit-device-configurations/edit-device-configurations.module.ts
+++ lightArduino/src/app/pages/edit-device-configurations/edit-device-configurations.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +13,8 @@ import { EditDeviceConfigurationsPage } from './edit-device-configurations.page'
     CommonModule,
     FormsModule,
     IonicModule,
-    EditDeviceConfigurationsPageRoutingModule
+    EditDeviceConfigurationsPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [EditDeviceConfigurationsPage]
 })
