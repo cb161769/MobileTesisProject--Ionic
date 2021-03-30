28d890802704755e25ef4815059e9abc2d4e7aa5
piercethevie <50924876+cb161769@users.noreply.github.com>
2021-03-29 23:22:06 -0400

    adding topic publishing

---
+++ lightArduino/src/app/data-services/MQTTService/mqttservice.service.ts
@@ -0,0 +1,21 @@
+  import { Injectable } from '@angular/core';
+import {Amplify,PubSub} from 'aws-amplify';
+import { AWSIoTProvider } from '@aws-amplify/pubsub';
+import { environment } from 'src/environments/environment';
+@Injectable({
+  providedIn: 'root'
+})
+export class MQTTServiceService {
+
+  constructor() { 
+    Amplify.addPluggable(new AWSIoTProvider({
+      aws_pubsub_region: environment.AWSIOTEndPoints.httpEndPoint,
+      aws_pubsub_endpoint:environment.AWSIOTEndPoints.region
+    }))
+
+  }
+
+  public async PublishToTopic(topicName:string){
+    return await PubSub.publish(topicName,{msg:topicName + 'subscribed'},{provider:'AWSIoTProvider'})
+  }
+}
