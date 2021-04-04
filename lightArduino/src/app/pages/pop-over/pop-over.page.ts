93cf817fb73f119e5ec5a57230d380214dd9774e
piercethevie <50924876+cb161769@users.noreply.github.com>
2021-04-04 00:22:19 -0400

    adding changes to close view

---
+++ lightArduino/src/app/pages/pop-over/pop-over.page.ts
@@ -0,0 +1,22 @@
+import { PopoverController } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pop-over',
+  templateUrl: './pop-over.page.html',
+  styleUrls: ['./pop-over.page.scss'],
+})
+export class PopOverPage implements OnInit {
+
+  constructor(private popoverController: PopoverController) { }
+  dataTitle:string = '';
+  data:any = [];
+  isDays:boolean = false;
+  ngOnInit() {
+  }
+  dismissPopOver(){
+    this.popoverController.dismiss();
+  }
+
+
+}
