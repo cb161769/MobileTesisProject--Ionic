c5b31f1d55948e87d24802a69a6a6c54a528d9b9
piercethevie <50924876+cb161769@users.noreply.github.com>
2021-04-24 00:13:47 -0400

    initial changes to security

---
+++ lightArduino/src/app/auth/auth.guard.ts
@@ -0,0 +1,48 @@
+import { Auth } from 'aws-amplify';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { CanActivate, CanActivateChild, CanDeactivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthStateService } from './auth-state.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanActivateChild, CanDeactivate<unknown>, CanLoad {
+  isLoggedIn = false;
+  /**
+   *
+   */
+  constructor(private router:Router, @Inject(PLATFORM_ID) private platform) {
+
+    
+  }
+  
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+     return Auth.currentAuthenticatedUser().then(_user =>{
+       return true;
+     }).catch(_err =>{
+       this.router.navigate(['/login'])
+       return false;
+     })
+    
+  }
+  canActivateChild(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+  canDeactivate(
+    component: unknown,
+    currentRoute: ActivatedRouteSnapshot,
+    currentState: RouterStateSnapshot,
+    nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    return true;
+  }
+}
