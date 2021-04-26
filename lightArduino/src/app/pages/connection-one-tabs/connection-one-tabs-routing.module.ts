1c21f10be76f01a10438b0745fbd649736d0da09
piercethevie <50924876+cb161769@users.noreply.github.com>
2021-04-25 21:11:06 -0400

    Autentication to child Routes

--- lightArduino/src/app/pages/connection-one-tabs/connection-one-tabs-routing.module.ts
+++ lightArduino/src/app/pages/connection-one-tabs/connection-one-tabs-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from 'src/app/auth/auth.guard';
 
 import { ConnectionOneTabsPage } from './connection-one-tabs.page';
 
@@ -7,26 +8,31 @@ const routes: Routes = [
   {
     path: 'connection-one-tab',
     component: ConnectionOneTabsPage,
+    canActivate:[AuthGuard],
     children: [
       {
         path:'tab1',
-        loadChildren:() => import('../connexion1/connexion1.module').then( m => m.Connexion1PageModule)
+        loadChildren:() => import('../connexion1/connexion1.module').then( m => m.Connexion1PageModule),
+        canActivate:[AuthGuard]
       },
       {
         path: 'tab2',
-        loadChildren:() => import('../connection-one-schedule/connection-one-schedule-routing.module').then( m => m.ConnectionOneSchedulePageRoutingModule)
+        loadChildren:() => import('../connection-one-schedule/connection-one-schedule-routing.module').then( m => m.ConnectionOneSchedulePageRoutingModule),
+        canActivate:[AuthGuard]
       },
         
       {
         path:'tab3',
-        loadChildren:() => import('../connection-one-statistics/connection-one-statistics.module').then( m => m.ConnectionOneStatisticsPageModule)
+        loadChildren:() => import('../connection-one-statistics/connection-one-statistics.module').then( m => m.ConnectionOneStatisticsPageModule),
+        canActivate:[AuthGuard]
       }
     ]
   },
   {
     path:'',
     redirectTo:'connection-one-tab/tab1',
-    pathMatch:'full'
+    pathMatch:'full',
+    canActivate:[AuthGuard]
   }
 ];
 
