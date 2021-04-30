1ca3eadf9533af297c027b25c183110c3a4f0c88
piercethevie <50924876+cb161769@users.noreply.github.com>
2021-04-30 15:19:08 -0400

    changes to review the comparisons beteen two

--- lightArduino/src/app/pages/connection-one-consumption-evolution/connection-one-consumption-evolution.page.ts
+++ lightArduino/src/app/pages/connection-one-consumption-evolution/connection-one-consumption-evolution.page.ts
@@ -11,5 +11,13 @@ export class ConnectionOneConsumptionEvolutionPage implements OnInit {
 
   ngOnInit() {
   }
+  doRefresh(event) {
+    console.log('Begin async operation');
+    
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+  }
 
 }
