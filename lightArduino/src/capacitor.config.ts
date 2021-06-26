e802c9422f289c93f092368684d81058a3f5ddf0
piercethevie <50924876+cb161769@users.noreply.github.com>
2021-06-26 00:58:38 -0400

    changes

---
+++ lightArduino/src/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.claudio.lightArduino',
+  appName: 'lightArduino',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
